5014936cee91abe227aa544fb435b7101c97cb60
git-Yamminipathy16 <yamminipathy2000@gmail.com>
2021-07-27 17:16:07 +0530

    Add project files.

---
+++ Csharpproject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharpproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
