c2b64a492a8604ab708cc2a8b9ef0bcc22bf2608
git-Yamminipathy16 <yamminipathy2000@gmail.com>
2021-07-28 09:32:24 +0530

    commit

---
+++ Csharpproject/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharpproject
+{
+    class Class1
+    {
+        static void display()
+        {
+            Console.WriteLine("HIIIIIIIIII");
+        }
+    }
+}
+
