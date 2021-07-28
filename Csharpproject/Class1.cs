acfaaa738960ec15a72cb63fac6c7a660411b726
git-Yamminipathy16 <yamminipathy2000@gmail.com>
2021-07-28 09:57:06 +0530

    commitnewmas

--- Csharpproject/Class1.cs
+++ Csharpproject/Class1.cs
@@ -9,7 +9,12 @@ namespace Csharpproject
         static void display()
         {
             Console.WriteLine("HIIIIIIIIII");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("did u see my bag");
+            Console.WriteLine("twice");
+            Console.WriteLine("make it right");
             int x = 20;
+            int u = 111;
         }
     }
 }
--- Csharpproject/Program.cs
+++ Csharpproject/Program.cs
@@ -8,6 +8,16 @@ namespace Csharpproject
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("You got the best of me");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You got the best of me");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You got the best of me");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You got the best of me");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You got the best of me");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("You got the best of me");
             int y = 20;
 
 
